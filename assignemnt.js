efc65f52269668a88668ed2291d21f0b11c2aac4
Hassanweb22 <you@example.com>
2020-07-17 09:40:22 +0500

    Chap3 39 to 67 Assignment

---
+++ assignemnt.js
@@ -0,0 +1,25 @@
+
+// CHapter#1
+
+const { array } = require("prop-types");
+
+function chap1_q1() {
+    alert("Welcome To Our Website");
+}
+
+function chap1_q2() {
+    alert("Error! PleaseEnter a Valid Password")
+}
+
+function chap1_q3() {
+    alert("Welcome to  JS Land... \nHappy Coding!")
+}
+
+function chap1_q4() {
+    alert("Welcome to  JS Land");
+}
+
+function chap1_q5() {
+    alert(`Hello... I can run JS through my web browser's onsole`);
+}
+
